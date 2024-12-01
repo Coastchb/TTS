053a338d6156bc031da6d1ab2a0d060e5f34be79
coastcao <coastcao@tencent.com>
2024-12-01 23:16:33 +0800

    modified SimplifyModel.py to only keep G model

--- SimplifyModel.py
+++ SimplifyModel.py
@@ -3,6 +3,7 @@ import os
 import openvino as ov
 #from notebook_utils import device_widget
 from pathlib import Path
+import torch
 
 sys.path.append('/root/coastcao/HelloTorch/TTS')
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
@@ -11,14 +12,10 @@ from TTS.tts.models.vits import Vits
 from TTS.config import load_config
 #import torch
 
-config_file_path = '../models/config.json'
-input_model_path = '../models/best_model.pth'
+model_dir = '/data/workspace/coastcao/HelloTorch/launch/python/models'
+config_file_path = '{0}/config.json'.format(model_dir)
+input_model_path = '{0}/best_model.pth'.format(model_dir)
 
-# output config
-onnx_model_path = Path('../models/coqui_vits.onnx')
-ir_model_path = onnx_model_path.with_suffix('.xml')
-
-# convert PyTorch model to ONNX model
 config = load_config(config_file_path)
 vits_model = Vits.init_from_config(config).cuda()
 vits_model.load_checkpoint(config=None, checkpoint_path=input_model_path)
@@ -27,3 +24,31 @@ print('loaded checkpoint')
 print('model info:')
 print(vits_model)
 
+if hasattr(vits_model, "module"):
+    print('model has module')
+    model_state = vits_model.module.state_dict()
+else:
+    print('model has not module')
+    model_state = vits_model.state_dict()
+
+print('model state dict:')
+
+disc_keys = []
+for key, value in model_state.items():
+    print(key, value)
+    if key.startswith('disc'):
+        disc_keys.append(key)
+        print('removed key: ', key)
+print(disc_keys)
+
+for key in disc_keys:
+    model_state.pop(key)
+
+
+state = {
+        "config": config,
+        "model": model_state,
+        "epoch": 0,
+    }
+
+torch.save(state, '{0}/G_model.pth'.format(model_dir))
